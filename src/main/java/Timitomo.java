3a07e5a94f31e842fa00208d1767dd45949b4ec0
Ryan Low <ryanlow02@gmail.com>
2025-01-28 06:01:23 +0800

    Add support for ToDo, Deadline, Event

--- src/main/java/Timitomo.java
+++ src/main/java/Timitomo.java
@@ -18,14 +18,15 @@ public class Timitomo {
         print("Goodbye!");
     }
 
-    private void addTask(String description) {
+    private void addTask(Task task) {
         if (taskCount >= MAX_TASKS) {
             print("Task overflow error! Focus on the current tasks first!");
             return;
         }
-        tasks[taskCount] = new Task(description);
+        tasks[taskCount] = task;
         taskCount++;
-        print("added: " + description);
+        print(String.format("I've added this task:%n  %s%nYou have %d %s in the list.",
+                task.toString(), taskCount, taskCount == 1 ? "task" : "tasks"));
     }
 
     private void printTasks() {
@@ -33,31 +34,35 @@ public class Timitomo {
             print("Nothing to do in task list. Stop slacking off!");
             return;
         }
+        System.out.printf("You have %d %s in the list. What are you waiting for?%n",
+                taskCount, taskCount == 1 ? "task" : "tasks");
         for (int i = 0; i < taskCount; i++) {
-            print(String.format("%d. %s", i+1, tasks[i]), i == taskCount - 1);
+            System.out.printf(">>> %d. %s%n", i+1, tasks[i]);
         }
+        System.out.println("----------------");
     }
 
-    private void markTask(int index) {
+    private void markTask(int index) throws IllegalArgumentException {
         if (index < 0 || index >= taskCount) {
             throw new IllegalArgumentException("Invalid task number!");
         }
         tasks[index].markAsDone();
-        print(String.format("Nice! I've marked it as done.%n    %s", tasks[index].toString()));
+        print(String.format("Nice! I've marked it as done.%n  %s", tasks[index].toString()));
     }
 
-    private void unmarkTask(int index) {
+    private void unmarkTask(int index) throws IllegalArgumentException {
         if (index < 0 || index >= taskCount) {
             throw new IllegalArgumentException("Invalid task number!");
         }
         tasks[index].markAsNotDone();
-        print(String.format("I've marked it as not done yet. Get to work!%n    %s", tasks[index].toString()));
+        print(String.format("I've marked it as not done yet. Get to work!%n  %s", tasks[index].toString()));
     }
 
     private void handleIO() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String[] input = scanner.nextLine().trim().split(" ");
+            String line = scanner.nextLine().trim();
+            String[] input = line.split(" ", 2);
             switch (input[0]) {
             case "":
                 break;
@@ -88,9 +93,33 @@ public class Timitomo {
                     print(e.getMessage());
                 }
                 break;
+            case "todo":
+                try {
+                    String description = input[1];
+                    addTask(new ToDo(description));
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    print("Missing description. What do you want to do?");
+                }
+                break;
+            case "deadline":
+                try {
+                    String[] args = input[1].split(" /by ", 2);
+                    addTask(new Deadline(args[0], args[1]));
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    print("I didn't quite catch that. What deadline do you have?");
+                }
+                break;
+            case "event":
+                try {
+                    String[] args = input[1].split(" /from ", 2);
+                    String[] times = args[1].split(" /to ", 2);
+                    addTask(new Event(args[0], times[0], times[1]));
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    print("I didn't quite catch that. What event do you have?");
+                }
+                break;
             default:
-                String description = String.join(" ", input);
-                addTask(description);
+                print("I don't know what that means.");
             }
         }
     }
@@ -100,15 +129,6 @@ public class Timitomo {
             System.out.println(">>> " + line);
         }
         System.out.println("----------------");
-    }
-
-    private void print(String text, boolean flag) {
-        for (String line : text.split(System.lineSeparator())) {
-            System.out.println(">>> " + line);
-        }
-        if (flag) {
-            System.out.println("----------------");
-        }
     }
 
     public static void main(String[] args) {
