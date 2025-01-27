92da896540f0b5091f60b82b2c592b39841c7057
Ryan Low <ryanlow02@gmail.com>
2025-01-27 19:42:38 +0800

    Add input echoing and exit

--- src/main/java/Timitomo.java
+++ src/main/java/Timitomo.java
@@ -1,17 +1,39 @@
+import java.util.Scanner;
+
 public class Timitomo {
     public Timitomo() {}
 
     private void greet() {
-        System.out.println("Hello! I'm Timitomo.\nWhat can I do for you?\n");
+        print("Hello! I'm Timitomo." + System.lineSeparator() + "What can I do for you?");
     }
 
     private void exit() {
-        System.out.println("Bye. Hope to see you again soon!");
+        print("Goodbye!");
+    }
+
+    private void handleIO() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                exit();
+                break;
+            }
+            print(input);
+        }
+        scanner.close();
+    }
+
+    private void print(String text) {
+        for (String line : text.split(System.lineSeparator())) {
+            System.out.println(">> " + line);
+        }
+        System.out.println("----------------");
     }
 
     public static void main(String[] args) {
         Timitomo bot = new Timitomo();
         bot.greet();
-        bot.exit();
+        bot.handleIO();
     }
 }
